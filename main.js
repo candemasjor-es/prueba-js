644fa4be264ab0c4e461f08d27b9a13b04a68330
Camilo Mascarell Jorques <camilomascarelljorques@gmail.com>
2025-02-05 10:40:56 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const elementoBotonAgredar = document.getElementById('botonAgredar');
+const elementoBotonSumar = document.getElementById('botonSumar');
+let sumar = 0;
+
+function botonAgredar(evento) {
+    evento.preventDefault();
+    let numero = document.getElementById('numero').value;
+    let lista = document.getElementById('lista');
+    let li = document.createElement('li');
+    li.innerText = 'Sumar numero: ' + numero;
+    lista.appendChild(li);
+    sumar += Number(numero);
+}
+function botonSumar() {
+    let contenedor = document.getElementById('contenido');
+    contenedor.innerText = 'El resultado de la sumatoria es ' + sumar;
+}
+elementoBotonAgredar.addEventListener('click', botonAgredar);
+elementoBotonSumar.addEventListener(`click`, botonSumar);
